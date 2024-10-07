2b5c4a6b5adf9a1299da44518faefc696113cb31
GitHub Action <action@github.com>
2024-10-07 11:34:20 +0000

    Template cleanup

--- src/test/kotlin/com/github/aragonerua/codecompletionfim/MyPluginTest.kt
+++ src/test/kotlin/com/github/aragonerua/codecompletionfim/MyPluginTest.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.aragonerua.codecompletionfim
 
 import com.intellij.ide.highlighter.XmlFileType
 import com.intellij.openapi.components.service
@@ -6,7 +6,7 @@ import com.intellij.psi.xml.XmlFile
 import com.intellij.testFramework.TestDataPath
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
 import com.intellij.util.PsiErrorElementUtil
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.aragonerua.codecompletionfim.services.MyProjectService
 
 @TestDataPath("\$CONTENT_ROOT/src/test/testData")
 class MyPluginTest : BasePlatformTestCase() {
